8e9bc63f6fc7eff645250fb0d51a8ea22c2a2ab9
hapheee <83578627+hapheee@users.noreply.github.com>
2024-12-07 16:43:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
+import base64
+import json
 
-# Firebase 인증 정보 가져오기
+# Streamlit에서 secrets 파일에 저장된 firebase_key를 가져오기
 encoded_firebase_key = st.secrets["firebase_key"]
 
 # Base64로 인코딩된 firebase_key 디코딩
-decoded_firebase_key = base64.b64decode(encoded_firebase_key)
+try:
+    decoded_firebase_key = base64.b64decode(encoded_firebase_key)
+    # 디코딩된 값 확인
+    st.write("디코딩된 값:", decoded_firebase_key)
 
-# 디코딩된 JSON 데이터를 파싱하여 Firebase 인증에 사용
-firebase_key = json.loads(decoded_firebase_key)
-
-# Firebase가 초기화되지 않은 경우에만 초기화
-if not firebase_admin._apps:
+    # 디코딩된 JSON 데이터를 파싱하여 Firebase 인증에 사용
+    firebase_key = json.loads(decoded_firebase_key)
+    
+    # Firebase 인증 정보로 초기화
     cred = credentials.Certificate(firebase_key)
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://opticmus-8f21c-default-rtdb.firebaseio.com/'
     })
+except Exception as e:
+    st.error(f"오류 발생: {e}")
 
 # Firebase에서 데이터 가져오기
 def get_data_from_firebase():
