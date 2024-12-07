4cbbb861a10d67aca99b88b31c388174df3c34c5
hapheee <83578627+hapheee@users.noreply.github.com>
2024-12-07 16:57:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,27 +15,6 @@ if not firebase_admin._apps:
    firebase_admin.initialize_app(cred, {           
            'databaseURL': 'https://opticmus-8f21c-default-rtdb.firebaseio.com/'
         })
-# # Firebase에서 데이터 가져오기
-# def get_data_from_firebase():
-#     ref = db.reference()  # 데이터 경로를 정확히 입력하세요.
-#     data = ref.get()  # 데이터 가져오기
-#     return data
-
-# # Streamlit에서 Firebase 데이터 표시
-# st.title("Firebase 실시간 데이터")
-
-# # Firebase 데이터 가져오기
-# data = get_data_from_firebase()
-
-# if data:
-#     st.write("Firebase에서 가져온 데이터:")
-#     st.json(data)  # 데이터가 있으면 JSON 형태로 띄움
-# else:
-
-
-
-
-
 # Firebase에서 데이터 가져오기
 def get_data_from_firebase():
     ref = db.reference()  # 데이터 경로를 정확히 입력하세요.
@@ -45,17 +24,12 @@ def get_data_from_firebase():
 # Streamlit에서 Firebase 데이터 표시
 st.title("Firebase 실시간 데이터")
 
-# 주기적으로 데이터 가져오기
+# Firebase 데이터 가져오기
 while True:
-    data = get_data_from_firebase()
+   data = get_data_from_firebase()
+   
+   if data:
+       st.write("Firebase에서 가져온 데이터:")
+       st.json(data)  # 데이터가 있으면 JSON 형태로 띄움
+   else:
 
-    if data:
-        st.write("Firebase에서 가져온 데이터:")
-        st.json(data)  # 데이터가 있으면 JSON 형태로 띄움
-    else:
-        st.write("데이터가 없습니다.")
-    
-    # 5초마다 데이터를 갱신하도록 설정
-    time.sleep(5)  # 5초마다 갱신
-    st.experimental_rerun()  # Streamlit 앱을 다시 실행시킴
-#     st.write("데이터가 없습니다.")
