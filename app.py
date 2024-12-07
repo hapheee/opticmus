825d2ae010c77cc22f050949f35d1c653bf6d9ed
hapheee <83578627+hapheee@users.noreply.github.com>
2024-12-07 16:22:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
-# import streamlit as st
-# # import firebase_admin
-# # from firebase_admin import credentials, db
-
-# # # # # Firebase 인증 정보 가져오기
-# # # firebase_config = st.secrets["firebase"]
-
-# # # JSON 키를 사용해 Firebase 초기화
-# # cred = credentials.Certificate("fiberbase_key")
-# # firebase_admin.initialize_app(cred, {
-# #     'databaseURL': 'https://opticmus-8f21c-default-rtdb.firebaseio.com/'
-# # })
-
-# # Firebase 데이터베이스에 접근
-# ref = db.reference()
-# data = ref.get()
-# st.write(data)
 import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
+import firebase_admin
+from firebase_admin import credentials, db
 
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
+# # # Firebase 인증 정보 가져오기
 
-st.pyplot(fig)
+# Firebase 인증 정보를 사용하여 초기화
+cred = credentials.Certificate(st.secrets["fiberbase_key.json"])
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://opticmus-8f21c-default-rtdb.firebaseio.com/'
+})
+
+# Firebase에서 데이터 가져오기
+def get_data_from_firebase():
+    # Firebase에서 특정 데이터를 가져오기 위한 참조
+    ref = db.reference()  # 데이터 경로를 정확히 입력하세요.
+    data = ref.get()  # 데이터 가져오기
+    return data
+
+# Streamlit에서 Firebase 데이터 표시
+st.title("Firebase 실시간 데이터")
+
+# Firebase 데이터 가져오기
+data = get_data_from_firebase()
+
+if data:
+    st.write("Firebase에서 가져온 데이터:")
+    st.json(data)  # 데이터가 있으면 JSON 형태로 띄움
+else:
+    st.write("데이터가 없습니다.")
+
+
