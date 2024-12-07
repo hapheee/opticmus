42fd476855d5f3e8b5c2c30156592a82f55a6cae
hapheee <83578627+hapheee@users.noreply.github.com>
2024-12-07 16:08:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
 
-# # Firebase 인증 정보 가져오기
-# firebase_config = st.secrets["firebase"]
+# # # Firebase 인증 정보 가져오기
+# # firebase_config = st.secrets["firebase"]
 
-# JSON 키를 사용해 Firebase 초기화
-cred = credentials.Certificate("fiberbase_key")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://opticmus-8f21c-default-rtdb.firebaseio.com/'
-})
+# # JSON 키를 사용해 Firebase 초기화
+# cred = credentials.Certificate("fiberbase_key")
+# firebase_admin.initialize_app(cred, {
+#     'databaseURL': 'https://opticmus-8f21c-default-rtdb.firebaseio.com/'
+# })
 
 # Firebase 데이터베이스에 접근
 ref = db.reference('some/data')
