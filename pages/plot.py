be6de79344cedd3481fa71708a42c3a671688506
hapheee <83578627+hapheee@users.noreply.github.com>
2024-12-10 21:13:56 +0900

    Update plot.py

--- pages/plot.py
+++ pages/plot.py
@@ -73,7 +73,7 @@ if data:
          ax.plot(wavelength, data[key], label=key, linewidth=1)
       ax.set_xlabel("Wavelength (nm)", color="white")  # x축 라벨
       ax.set_ylabel("Fluorescence intensity", color="white")  # y축 라벨
-      ax.legend(title="Wells", labelcolor = 'white',facecolor='#1e1e1e', edgecolor='white', labelcolor='white')
+      ax.legend(facecolor='#1e1e1e', edgecolor='white', labelcolor='white')
       ax.set_xlim(x_min, x_max)
       ax.set_ylim(y_min, y_max)
       ax.set_yticks(np.arange(y_min, y_max, y_max/10)) 
@@ -91,7 +91,7 @@ if data:
       ax.set_xlim(x_min, x_max)
       ax.set_ylim(y_min, y_max)
       ax.set_yticks(np.arange(y_min, y_max, y_max/10)) 
-      ax.legend(title="Wells", labelcolor = 'white', facecolor='white', edgecolor='white', labelcolor='white')
+      ax.legend(facecolor='white', edgecolor='white', labelcolor='white')
       ax.tick_params(axis='y', colors='white')  # y축 눈금 및 레이블 색상
       ax.tick_params(axis='x', colors='white') 
       ax.grid(axis='y', color='white', linestyle='--', linewidth=0.5)
