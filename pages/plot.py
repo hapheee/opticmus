af2505340cd3758f86540813883f02b1a23ca01d
hapheee <83578627+hapheee@users.noreply.github.com>
2024-12-10 16:04:00 +0900

    Update plot.py

--- pages/plot.py
+++ pages/plot.py
@@ -1,1 +1,33 @@
+import streamlit as st
+import firebase_admin
+from firebase_admin import credentials, db
+import base64
+import json
 
+# Streamlit에서 secrets 파일에 저장된 firebase_key를 가져오기
+encoded_firebase_key = st.secrets["firebase_key"]
+decoded_firebase_key = base64.b64decode(encoded_firebase_key)
+firebase_key = json.loads(decoded_firebase_key)
+
+# Initialize 
+if not firebase_admin._apps:
+   cred = credentials.Certificate(firebase_key)
+   firebase_admin.initialize_app(cred, {           
+           'databaseURL': 'https://opticmus-8f21c-default-rtdb.firebaseio.com/'
+        })
+   
+# Firebase에서 데이터 가져오기
+def get_data_from_firebase():
+    ref = db.reference('plate_reader_data')  # firebase에 저장해둔 경로 
+    data = ref.get()  # 데이터 가져오기
+    return data
+
+# Streamlit에서 Firebase 데이터 표시
+st.set_page_config(page_title="Plate Reader Data", layout="wide", page_icon="📈")
+st.markdown("# Plotting Demo")
+st.sidebar.header("Plotting Demo")
+st.write(
+    """This demo illustrates a combination of plotting and animation with
+Streamlit. We're generating a bunch of random numbers in a loop for around
+5 seconds. Enjoy!"""
+)
